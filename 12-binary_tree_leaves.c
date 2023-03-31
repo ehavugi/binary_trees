ec779bbafa9d69f3b6d38165950c001b1616266e
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-03-30 21:00:05 -0700

    added more

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,6 +12,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	/* to be implemented */
 	if (tree == NULL)
 		return (0);
-	else
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
+	return (binary_tree_leaves(tree->left) +
+		binary_tree_leaves(tree->right));
 }
--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -3,6 +3,36 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_leaves_ - func return uncle
+ * @tree: node to find uncle for
+ * Return: 1 or 0
+ */
+size_t binary_tree_leaves_(const binary_tree_t *tree)
+{
+	/* to be implemented */
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	return (binary_tree_leaves_(tree->left) +
+		binary_tree_leaves_(tree->right));
+}
+
+/**
+ * binary_tree_size_ - func return uncle
+ * @tree: node to find uncle for
+ * Return: 1 or 0
+ */
+size_t binary_tree_size_(const binary_tree_t *tree)
+{
+        /* to be implemented */
+        if (tree == NULL)
+                return (0);
+        else
+                return (binary_tree_size_(tree->left) + 1 +
+                        binary_tree_size_(tree->right));
+}
+/**
  * binary_tree_nodes - func return uncle
  * @tree: node to find uncle for
  * Return: 1 or 0
@@ -10,8 +40,5 @@
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	/* to be implemented */
-	if (tree == NULL)
-		return (0);
-	else
-		return (1);
+	return (binary_tree_size_(tree) - binary_tree_leaves_(tree));
 }
